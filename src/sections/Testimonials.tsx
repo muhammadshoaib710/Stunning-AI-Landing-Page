f83e464bd87a2e4005f6d306d02a907f5914f61a
SHOAIB UR REHMAN <01-135202-090@student.bahria.edu.pk>
2024-10-31 23:57:14 +0500

    Add CallToAction section and enhance Testimonials layout

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -33,23 +33,41 @@ const testimonials = [
 
 export const Testimonials = () => {
   return (
-    <section className="py-20">
+    <section className="py-20 md:py-24">
       <div className="container">
-        <h2 className="text-5xl text-center tracking-tighter font-medium">Beyond Expectation</h2>
-        <p className="text-white/70 text-lg text-center mt-5 tracking-tight" >
+        <h2 className="text-5xl  md:text-6xl text-center tracking-tighter font-medium">
+          Beyond Expectation
+        </h2>
+        <p className="text-white/70 text-lg md:text-xl text-center mt-5 tracking-tight max-w-sm mx-auto">
           Our revolutionary AI tools have transformed our clients' strategies
         </p>
-        {testimonials.map((testimonial) => (
-          <div key={testimonial.name} className="border border-white/15 p-6 rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)]">
-            <div>{testimonial.text}</div>
-            <Image
-              src={testimonial.avatarImg}
-              alt={`Avatar for ${testimonial.name}`}
-            />
-            <div>{testimonial.name}</div>
-            <div>{testimonial.title}</div>
+        <div className="overflow:hidden mt-10 [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+          <div className="flex gap-5">
+            {testimonials.map((testimonial) => (
+              <div
+                key={testimonial.name}
+                className="border border-white/15 p-6 md:p-10  rounded-xl bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-xs md:max-w-md flex-none"
+              >
+                <div className="text-lg  tracking-tight md:text-xl">{testimonial.text}</div> 
+                <div className="flex items-center gap-3 mt-5">
+                  <div className="relative after:content-[''] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:mix-blend-soft-light before:content-[''] before:absolute before:inset-0 before:before before:border-white/30 before:z-10 rounded-lg">
+                    <Image
+                      src={testimonial.avatarImg}
+                      alt={`Avatar for ${testimonial.name}`}
+                      className="h-11 w-11 rounded-lg grayscale "
+                    />
+                  </div>
+                  <div className="">
+                    <div>{testimonial.name}</div>
+                    <div className="text-white/50 text-sm">
+                      {testimonial.title}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
       </div>
     </section>
   );
